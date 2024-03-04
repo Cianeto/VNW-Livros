5e95e74e499e00c5e8da9d7b359b45651f4b0b21
Gabriel Melo Dias <97137926+Cianeto@users.noreply.github.com>
2024-03-04 16:14:21 -0300

    -

---
+++ desafio_VaiNuBanco/src/vnw/bank/contas/ContaBancaria.java
@@ -0,0 +1,26 @@
+package vnw.bank.contas;
+
+import java.util.HashSet;
+
+public interface ContaBancaria {
+	
+	void sacar(ContaBancaria conta, HashSet<ContaBancaria> contas);
+	void depositar(ContaBancaria conta, HashSet<ContaBancaria> contas);
+	void transferir(ContaBancaria conta, HashSet<ContaBancaria> contas);
+	
+	int getId();
+	String getAgencia();
+	String getTitular();
+	String getCpf();
+	String getSenha();
+	double getSaldo();
+	double getLimiteDeCredito();
+	int getbDay();
+	void setTitular(String titular);
+	void setCpf(String cpf);
+	void setSenha(String senha);
+	void setLimiteDeCredito(double limiteDeCredito);
+	void setbDay(int bDay);
+	void setSaldo(double saldo);
+	String toString();
+}
